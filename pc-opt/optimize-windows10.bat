476f35287785e865c064c34c7065112901be8d44
tobiasrepracek <tobias.repracek.tr@gmail.com>
2023-02-05 17:31:00 +0100

    c

---
+++ pc-opt/optimize-windows10.bat
@@ -0,0 +1,8 @@
+@echo off
+title Optimization - Win10
+color 0a
+cls
+echo Running optimization by Levna-Elektronika.eu
+echo This optimization file is open source.
+echo You can view it on https://curl.levna-elektronika.eu/pc-opt/optimize-windows10.bat
+ping localhost -n 100 > nul
